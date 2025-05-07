eeb81ecfc1471790a55982d8c543a9c62af76c9a
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-05-07 22:13:11 +0900

    feat : 회원 엔티티 추가

---
+++ src/main/java/com/example/weterview/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.weterview.entity;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "nickname", length = 100)
+    private String nickname;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "withdrawn_at", updatable = false)
+    private LocalDateTime withdrawnAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
