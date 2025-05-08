d799e822cc71ef9595481b7fd0e6b107e56b5a81
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-05-08 22:06:59 +0900

    feat : 생성, 수정일 기록을 위한 annotation 추가

--- src/main/java/com/example/weterview/WeterviewApplication.java
+++ src/main/java/com/example/weterview/WeterviewApplication.java
@@ -2,8 +2,10 @@ package com.example.weterview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WeterviewApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeterviewApplication.class, args);
