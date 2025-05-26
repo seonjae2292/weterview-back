738cafa458306e5641932520bdaebc2092d00461
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-05-26 21:07:57 +0900

    feat : 카카오 로그인 로직 개선

--- src/main/java/com/example/weterview/controller/OAuthController.java
+++ src/main/java/com/example/weterview/controller/OAuthController.java
@@ -1,13 +1,10 @@
 package com.example.weterview.controller;
 
+import com.example.weterview.dto.common.ApiResponse;
 import com.example.weterview.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -18,8 +15,7 @@ public class OAuthController {
     private final OAuthService oAuthService;
 
     @GetMapping("/kakao/callback")
-    public Mono<ResponseEntity<String>> getAuthorizeToken(@RequestParam("code") String code) {
-        return oAuthService.postVerifyUserToKakao(code)
-                .map(result -> ResponseEntity.ok(result));
+    public Mono<ApiResponse<?>> getAuthorizeToken(@RequestParam("code") String code) {
+        return oAuthService.postVerifyUserToKakao(code);
     }
 }
