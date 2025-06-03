c25d4fe51b91f9f3191e440bf0c4040b4d0e7b5e
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-03 21:15:14 +0900

    feat : return type 추가

--- src/main/java/com/example/weterview/controller/OAuthController.java
+++ src/main/java/com/example/weterview/controller/OAuthController.java
@@ -2,6 +2,7 @@ package com.example.weterview.controller;
 
 import com.example.weterview.dto.SignupInfoDto;
 import com.example.weterview.dto.common.ApiResponse;
+import com.example.weterview.dto.common.OurMemberDto;
 import com.example.weterview.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +20,7 @@ public class OAuthController {
     private final OAuthService oAuthService;
 
     @GetMapping("/kakao/callback")
-    public Mono<ApiResponse<?>> getAuthorizeToken(@RequestParam("code") String code) {
+    public Mono<ApiResponse<? extends OurMemberDto>> getAuthorizeToken(@RequestParam("code") String code) {
         return oAuthService.postVerifyUserToKakao(code);
     }
 
