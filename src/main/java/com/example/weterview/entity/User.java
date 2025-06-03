db9f290efa936bdf3a527a03a3acd8d5d993574f
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-03 20:34:27 +0900

    feat : 회원 추가시 createAt 자동생성 annotation 추가

--- src/main/java/com/example/weterview/entity/User.java
+++ src/main/java/com/example/weterview/entity/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Data
 @Table(name = "users")
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
