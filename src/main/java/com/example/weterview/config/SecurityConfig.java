931c9c98e67c6a834b1903363d97309f5c80c59a
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-03 20:35:52 +0900

    feat : 비밀번호 암호화 메서드 추가

--- src/main/java/com/example/weterview/config/SecurityConfig.java
+++ src/main/java/com/example/weterview/config/SecurityConfig.java
@@ -7,17 +7,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -28,4 +24,10 @@ public class SecurityConfig {
                         "/oauth/**", "/auth/**").permitAll().anyRequest().authenticated());
         return http.build();
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
