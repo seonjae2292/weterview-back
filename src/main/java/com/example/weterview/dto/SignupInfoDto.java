bb29e7234d98bf975555e5aa637e0fdf06a52b40
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-03 20:35:09 +0900

    feat : kakaoAuthTime 삭제 및 kakaoUserNumber에 @NotBlank annotation 추가

--- src/main/java/com/example/weterview/dto/SignupInfoDto.java
+++ src/main/java/com/example/weterview/dto/SignupInfoDto.java
@@ -6,17 +6,21 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 public class SignupInfoDto {
-    private String kakaoUserNumber; // optional
-    private String kakaoAuthTime; // optional
+    @NotBlank
+    private String kakaoUserNumber;
+
     @NotBlank
     @Pattern(regexp = "^[가-힣]{2,10}$", message = "The name must be 2 to 10 Korean characters.")
     private String name;
+
     @NotBlank
     @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,30}$", message = "Nickname must be 2 to 30 characters long and contain only Korean letters, English letters, or digits.")
     private String nickname;
+
     @NotBlank
     @Email
     private String email;
+
     @NotBlank
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+{}\\[\\]|\\\\;:'\",.<>/?]).{8,}$")
     @Max(value = 100, message = "The password can be up to 100 digits")
