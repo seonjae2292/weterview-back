6d4df5e5bfa5e7c7e92f414613b99eab299e86a3
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-14 21:51:01 +0900

    feat : 스터디그룹 조회 응답 dto 추가

---
+++ src/main/java/com/example/weterview/dto/studyGroup/response/GetStudyGroupPageRes.java
@@ -0,0 +1,25 @@
+package com.example.weterview.dto.studyGroup.response;
+
+import com.example.weterview.enums.studyGroup.FieldEnum;
+import com.example.weterview.enums.studyGroup.LocationEnum;
+import com.example.weterview.enums.studyGroup.StatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class GetStudyGroupPageRes {
+    private FieldEnum field;
+    private StatusEnum status;
+    private String title;
+    private String subTitle;
+    private LocationEnum location;
+    private String description;
+    private Integer recruitingNumber;
+    private Integer totalNumber;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+}
