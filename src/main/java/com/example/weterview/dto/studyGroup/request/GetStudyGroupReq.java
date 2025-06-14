7b80d558feee73694df16164d3f2fba1d3a36f34
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-14 21:52:43 +0900

    feat : 스터디그룹 조회 요청 dto 추가

---
+++ src/main/java/com/example/weterview/dto/studyGroup/request/GetStudyGroupReq.java
@@ -0,0 +1,17 @@
+package com.example.weterview.dto.studyGroup.request;
+
+import com.example.weterview.dto.common.PagiBasicDto;
+import com.example.weterview.enums.studyGroup.FieldEnum;
+import com.example.weterview.enums.studyGroup.LocationEnum;
+import com.example.weterview.enums.studyGroup.StatusEnum;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class GetStudyGroupReq extends PagiBasicDto {
+    private String title;
+    private FieldEnum field;
+    private LocationEnum location;
+    private StatusEnum status;
+}
