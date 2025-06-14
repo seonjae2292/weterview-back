38427dbfc1e353de097bc0da7899259fd910e406
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-14 21:51:25 +0900

    feat : 조회 Pagination 공통 dto 추가

---
+++ src/main/java/com/example/weterview/dto/common/PagiBasicDto.java
@@ -0,0 +1,14 @@
+package com.example.weterview.dto.common;
+
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PagiBasicDto {
+    @Min(value = 1, message = "page는 1 이상이어야 합니다.")
+    private int pageNumber = 1;
+    @Min(value = 1, message = "size는 1 이상이어야 합니다.")
+    private int pageSize = 10;
+}
