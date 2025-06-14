89b6a049d829b72bea207fd8cf6bba15b44c16e2
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-14 21:51:48 +0900

    feat : Pagination을 위한

--- src/main/java/com/example/weterview/repository/StudyGroupRepository.java
+++ src/main/java/com/example/weterview/repository/StudyGroupRepository.java
@@ -2,8 +2,11 @@ package com.example.weterview.repository;
 
 import com.example.weterview.entity.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long> {
+public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long>,
+        JpaSpecificationExecutor<StudyGroup> {
 }
+
