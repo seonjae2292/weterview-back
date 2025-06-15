33a1ddb9a5d81f5e7b0b9faccced4619ee7c2fcb
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-15 20:05:53 +0900

    feat : studygroup statusenum에 deleted 추가

--- src/main/java/com/example/weterview/enums/studyGroup/StatusEnum.java
+++ src/main/java/com/example/weterview/enums/studyGroup/StatusEnum.java
@@ -2,7 +2,8 @@ package com.example.weterview.enums.studyGroup;
 
 public enum StatusEnum {
     RECRUITING("모집중"),
-    CLOSED("모집마감");
+    CLOSED("모집마감"),
+    DELETED("삭제됨");
 
     StatusEnum(String displayName) {
     }
