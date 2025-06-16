75d8b8be45798edce827bef259bed828809f78e5
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-16 21:00:20 +0900

    feat : StudyMembershipRepository에 스터디그룹-유저로 중복 참가 여부 조회 메서드 추가

--- src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
+++ src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
@@ -1,9 +1,14 @@
 package com.example.weterview.repository;
 
 import com.example.weterview.entity.StudyGroup;
+import com.example.weterview.entity.StudyMembership;
+import com.example.weterview.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface StudyMembershipRepository extends JpaRepository<StudyGroup, Long> {
+public interface StudyMembershipRepository extends JpaRepository<StudyMembership, Long> {
+    Optional<StudyMembership> findByStudyGroupIdAndUserId(StudyGroup studyGroupId, User userId);
 }
