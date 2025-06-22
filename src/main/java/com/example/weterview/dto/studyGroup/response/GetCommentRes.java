fddc50c82fa76e12d86870c20541bbd69d1e8384
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-06-22 16:27:27 +0900

    feat : 댓글 조회 dto

---
+++ src/main/java/com/example/weterview/dto/studyGroup/response/GetCommentRes.java
@@ -0,0 +1,14 @@
+package com.example.weterview.dto.studyGroup.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class GetCommentRes {
+    private String content;
+    private LocalDateTime createdAt;
+    private String nickname;
+}
