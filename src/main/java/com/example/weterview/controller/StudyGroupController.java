38b614c4e254364c645735361c5a66dd325a219c
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-07-21 20:29:50 +0900

    chore : 주석 추가

--- src/main/java/com/example/weterview/controller/StudyGroupController.java
+++ src/main/java/com/example/weterview/controller/StudyGroupController.java
@@ -13,21 +13,25 @@ import org.springframework.web.bind.annotation.*;
 public class StudyGroupController {
     private final StudyGroupService studyGroupService;
 
+    // 스터디 그룹 모집 게시글 생성
     @PostMapping("/create")
     public ApiResponse<?> createStudyGroup(@RequestBody CreateStudyGroupReq req) {
         return studyGroupService.createStudyGroup(req);
     }
 
+    // 스터디 그룹 모집 게시글 검색 조회
     @GetMapping("/get")
     public ApiResponse<?> getStudyGroup(@ModelAttribute @Valid GetStudyGroupReq req) {
         return studyGroupService.getStudyGroup(req);
     }
 
+    // 스터디 그룹 모집 게시글 단건 조회
     @GetMapping("/get/{id}")
     public ApiResponse<GetStudyGroupByIdRes> getStudyGroupById(@PathVariable String id) {
         return studyGroupService.getStudyGroupById(id);
     }
 
+    // 스터디 그룹 모집 게시글 수정
     @PatchMapping("/update/{id}")
     public ApiResponse<?> updateStudyGroup(
             @PathVariable String id,
@@ -35,11 +39,13 @@ public class StudyGroupController {
         return studyGroupService.updateStudyGroup(id, req);
     }
 
+    // 스터디 그룹 모집 게시글 삭제
     @DeleteMapping("/delete/{id}")
     public ApiResponse<?> deleteStudyGroup(@PathVariable String id) {
         return studyGroupService.deleteStudyGroup(id);
     }
 
+    // 스터디 그룹 모집 게시글 참여하기
     @PostMapping("/join")
     public ApiResponse<?> joinStudyGroup(
             @RequestBody JoinStudyGroupReq req,
@@ -47,6 +53,7 @@ public class StudyGroupController {
         return studyGroupService.joinStudyGroup(req, jwt);
     }
 
+    // 댓글 추가
     @PostMapping("/create/comment")
     public ApiResponse<?> createComment(
             @RequestBody CreateCommentReq req,
@@ -54,8 +61,25 @@ public class StudyGroupController {
         return studyGroupService.createComment(req, jwt);
     }
 
+    // 스터디 그룹 모집 게시글 댓글 조회
     @GetMapping("/get/comment/{studyGroupId}")
     public ApiResponse<?> getComment(@PathVariable String studyGroupId) {
         return studyGroupService.getComment(studyGroupId);
+    }
+
+    // 스터디 그룹 모집 게시글 좋아요
+    @PostMapping("/{studyGroupId}/likes")
+    public ApiResponse<?> likeStudyGroup(
+            @PathVariable String studyGroupId,
+            @RequestHeader("Authorization") String jwt) {
+        return studyGroupService.likeStudyGroup(studyGroupId, jwt);
+    }
+
+    // 스터디 그룹 모집 게시글 좋아요 취소
+    @DeleteMapping("/{studyGroupId}/likes")
+    public ApiResponse<?> unlikeStudyGroup(
+            @PathVariable String studyGroupId,
+            @RequestHeader("Authorization") String jwt){
+        return studyGroupService.unlikeStudyGroup(studyGroupId, jwt);
     }
 }
