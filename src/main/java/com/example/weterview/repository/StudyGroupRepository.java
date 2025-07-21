f447e05f6c4c0b2b57478097cd096156551a4bd3
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-07-21 20:29:40 +0900

    feat : StudyGroupRepository findByUser 메서드 추가

--- src/main/java/com/example/weterview/repository/StudyGroupRepository.java
+++ src/main/java/com/example/weterview/repository/StudyGroupRepository.java
@@ -1,6 +1,8 @@
 package com.example.weterview.repository;
 
-import com.example.weterview.entity.StudyGroup;
+import com.example.weterview.entity.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long>,
         JpaSpecificationExecutor<StudyGroup> {
+    Page<StudyGroup> findByUser(User user, Pageable pageable);
 }
 
