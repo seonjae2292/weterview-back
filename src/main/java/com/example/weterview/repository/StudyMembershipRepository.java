3ee6a5d51870afe55d811d811d59723a0c78ecca
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-07-21 20:28:33 +0900

    feat : StudyMembershipRepository findByUserId 메서드 추가

--- src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
+++ src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
@@ -6,9 +6,12 @@ import com.example.weterview.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StudyMembershipRepository extends JpaRepository<StudyMembership, Long> {
     Optional<StudyMembership> findByStudyGroupIdAndUserId(StudyGroup studyGroupId, User userId);
+    Optional<List<StudyMembership>> findByUserId(User user);
 }
