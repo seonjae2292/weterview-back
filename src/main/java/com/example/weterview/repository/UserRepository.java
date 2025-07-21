b9e79fdec0dda3522dd38c21a606bd3061daed1e
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-07-21 20:27:28 +0900

    remove : existsByKakaoUserNumber 메서드 삭제

--- src/main/java/com/example/weterview/repository/UserRepository.java
+++ src/main/java/com/example/weterview/repository/UserRepository.java
@@ -9,6 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByKakaoUserNumber(String kakaoUserNumber);
-    boolean existsByKakaoUserNumber(String kakaoUserNumber);
     boolean existsByNickname(String nickname);
 }
