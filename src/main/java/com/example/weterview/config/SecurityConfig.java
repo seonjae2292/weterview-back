405641917840f7a8b3a254564e93a8dd31d12d52
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-07-22 21:50:14 +0900

    feat : swagger는 토큰 없이 가능하게

--- src/main/java/com/example/weterview/config/SecurityConfig.java
+++ src/main/java/com/example/weterview/config/SecurityConfig.java
@@ -20,7 +20,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final JwtUtil jwtUtil;
 
+    private static final String[] OAUTH_URL = {
+            "/oauth/**",
+            "/auth/**"
+    };
 
+    private static final String[] SWAGGER_URL = {
+            "/v3/api-docs/**", // Swagger UI 리소스
+            "/swagger-ui/**", // Swagger UI 페이지
+            "/swagger-ui.html" // OpenAPI 명세서
+    };
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
@@ -33,13 +42,11 @@ public class SecurityConfig {
                 .cors(cors -> cors.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers(
-                        "/oauth/**",
-                        "/auth/**",
-                        "/swagger-ui/**",       // Swagger UI 리소스
-                        "/swagger-ui.html",     // Swagger UI 페이지
-                        "/v3/api-docs/**"       // OpenAPI 명세서
-                ).permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(OAUTH_URL).permitAll()
+                                .requestMatchers(SWAGGER_URL).permitAll()
+                        .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
