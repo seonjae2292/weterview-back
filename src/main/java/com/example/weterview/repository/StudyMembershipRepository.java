d2ff32397b347c0db199a1b5ae1c93b0757a8ce9
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-07-22 21:24:30 +0900

    fix : JPQL 사용해서 N + 1 문제 해결

--- src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
+++ src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
@@ -3,15 +3,20 @@ package com.example.weterview.repository;
 import com.example.weterview.entity.StudyGroup;
 import com.example.weterview.entity.StudyMembership;
 import com.example.weterview.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StudyMembershipRepository extends JpaRepository<StudyMembership, Long> {
     Optional<StudyMembership> findByStudyGroupIdAndUserId(StudyGroup studyGroupId, User userId);
-    Optional<List<StudyMembership>> findByUserId(User user);
+
+    @Query("select m.studyGroup from StudyMembership m where m.user = : user")
+    Page<StudyGroup> findByUserWithStudyGroup(@Param("user") User user, Pageable pageable);
 }
