bef68f9a16b28b7d5ffa752cfd81f0f5baff4632
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-18 13:12:23 +0900

    remove : authtime, nickname

--- src/main/java/com/example/weterview/dto/KakaoIdTokenInfoRes.java
+++ src/main/java/com/example/weterview/dto/KakaoIdTokenInfoRes.java
@@ -4,10 +4,8 @@ import lombok.Data;
 
 @Data
 public class KakaoIdTokenInfoRes {
-    private String sub;
-    /** UNIX Timestamp */
-    /// https://developers.kakao.com/docs/latest/ko/kakaologin/utilize
-    private Integer authTime;
-    private String nickname;
-    private String email;
+    private String sub; // ID 토큰에 해당하는 사용자의 회원번호
+    private String email; // kakao
 }
+
+/// https://developers.kakao.com/docs/latest/ko/kakaologin/utilize
