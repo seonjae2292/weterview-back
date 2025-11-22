186a7a1b3671c77d13593f8859bdbca1be7ba619
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-22 15:55:15 +0900

    fix: kakaoUserNumber -> User -> StudyMembership -> StudyGroup

--- src/main/java/com/example/weterview/repository/StudyGroupRepository.java
+++ src/main/java/com/example/weterview/repository/StudyGroupRepository.java
@@ -5,11 +5,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long>,
         JpaSpecificationExecutor<StudyGroup> {
     Page<StudyGroup> findByUser(User user, Pageable pageable);
+    // Users 엔티티에서 kakaonum으로 user id 조회
+    // studygroupmembership에서 user id로 조회
+    // 조회한 게시글 id로 studygroup에서 조회
+    @Query("SELECT sg FROM StudyGroup sg WHERE sg.id = (SELECT s.studyGroup.id FROM StudyMembership s WHERE s.user.id = (SELECT u.id FROM User u WHERE u.kakaoUserNumber = :kakaoNum))")
+    Page<StudyGroup> findByKakaonum(@Param("kakaoNum") String kakaoNum, Pageable pageable);
 }
+
+
 
