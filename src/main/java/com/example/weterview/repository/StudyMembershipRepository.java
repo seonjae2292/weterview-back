0433be3d36ebe69c57c6862690c1a61d151a2038
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-25 22:11:10 +0900

    chore: 불필요한 코드 삭제

--- src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
+++ src/main/java/com/example/weterview/repository/StudyMembershipRepository.java
@@ -1,13 +1,7 @@
 package com.example.weterview.repository;
 
-import com.example.weterview.entity.StudyGroup;
 import com.example.weterview.entity.StudyMembership;
-import com.example.weterview.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,13 +10,4 @@ import java.util.Optional;
 @Repository
 public interface StudyMembershipRepository extends JpaRepository<StudyMembership, Long> {
     Optional<StudyMembership> findById(Long studyGroupId);
-    Optional<StudyMembership> findByStudyGroupIdAndUserId(StudyGroup studyGroupId, User userId);
-
-    @Query(value = "select User " +
-            "from User u " +
-            "where u.id in (select m.user from StudyMembership m where m.studyGroup = : studyGroupId)", nativeQuery = true)
-    Optional<List<User>> findByStudyGroupId(long studyGroupId);
-
-    @Query("select m.studyGroup from StudyMembership m where m.user = : user")
-    Page<StudyGroup> findByUserWithStudyGroup(@Param("user") User user, Pageable pageable);
 }
