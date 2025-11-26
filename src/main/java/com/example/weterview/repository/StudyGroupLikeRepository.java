c3fdf6cbd611ad8b59983398f39ef1c787665e39
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-26 16:35:54 +0900

    feat: StudyGroupLikes 엔티티에서 User 객체로 조회

--- src/main/java/com/example/weterview/repository/StudyGroupLikeRepository.java
+++ src/main/java/com/example/weterview/repository/StudyGroupLikeRepository.java
@@ -1,8 +1,11 @@
 package com.example.weterview.repository;
 
+import com.example.weterview.dto.myPage.response.GetLikedPostRes;
 import com.example.weterview.entity.StudyGroup;
 import com.example.weterview.entity.StudyGroupLike;
 import com.example.weterview.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,6 @@ import java.util.Optional;
 @Repository
 public interface StudyGroupLikeRepository extends JpaRepository<StudyGroupLike, Long> {
     Optional<StudyGroupLike> findByStudyGroupAndUser(StudyGroup studyGroup, User user);
+
+    Page<StudyGroupLike> findStudyGroupLikesByUser(User user, Pageable pageable);
 }
