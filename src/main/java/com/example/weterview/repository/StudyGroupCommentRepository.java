98cc5538e0d06de21ce7085eb855d70c16a5a337
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-26 17:07:25 +0900

    feat: studyGroupComment 엔티티에서 User 객체로 조회하는 메서드 추가

--- src/main/java/com/example/weterview/repository/StudyGroupCommentRepository.java
+++ src/main/java/com/example/weterview/repository/StudyGroupCommentRepository.java
@@ -2,6 +2,9 @@ package com.example.weterview.repository;
 
 import com.example.weterview.entity.StudyGroup;
 import com.example.weterview.entity.StudyGroupComment;
+import com.example.weterview.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,6 @@ import java.util.List;
 @Repository
 public interface StudyGroupCommentRepository extends JpaRepository<StudyGroupComment,Long> {
     List<StudyGroupComment> findByStudyGroupId(Long studyGroup_id);
+
+    Page<StudyGroupComment> findStudyGroupCommentsByUser(User user, Pageable pageable);
 }
