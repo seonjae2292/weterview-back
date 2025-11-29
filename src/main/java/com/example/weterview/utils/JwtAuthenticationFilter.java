966cc5b4639d50194b4c78c8815d938e1d9cdd83
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-29 13:33:53 +0900

    feat: DB에서 조회를 이 클래스에서 수행하도록

--- src/main/java/com/example/weterview/utils/JwtAuthenticationFilter.java
+++ src/main/java/com/example/weterview/utils/JwtAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package com.example.weterview.utils;
 
+import com.example.weterview.config.CustomUserDetails;
+import com.example.weterview.entity.User;
+import com.example.weterview.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,6 +17,7 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
     @Override
@@ -22,10 +27,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
         String token = jwtUtil.resolveToken(request);
+
         if (token != null && jwtUtil.isTokenExpired(token)) {
-            Authentication auth = jwtUtil.createAuthentication(token);
+            String kakaoUserNumber = jwtUtil.getKakaoUserNumFromToken(token);
+
+            User user = userRepository.findByKakaoUserNumber(kakaoUserNumber)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자 입니다."));
+
+            CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+//            Authentication auth = jwtUtil.createAuthentication(token);
+
+            Authentication auth =
+                    new UsernamePasswordAuthenticationToken(
+                            customUserDetails,
+                            null,
+                            customUserDetails.getAuthorities());
+
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
+
         // **반드시** 다음 필터로 요청을 넘겨줘야 함
         filterChain.doFilter(request, response);
     }
