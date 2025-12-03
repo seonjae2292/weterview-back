23c21feb6b99166ea203e5f8ed0d1c2370dd5f96
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-03 16:02:47 +0900

    feat: 인기있는 스터디 그룹 조회 api 추가

--- src/main/java/com/example/weterview/repository/StudyGroupRepository.java
+++ src/main/java/com/example/weterview/repository/StudyGroupRepository.java
@@ -1,6 +1,7 @@
 package com.example.weterview.repository;
 
 import com.example.weterview.entity.*;
+import com.example.weterview.enums.studyGroup.StatusEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,9 +19,16 @@ public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long>,
     // Users 엔티티에서 kakaonum으로 user id 조회
     // studygroupmembership에서 user id로 조회
     // 조회한 게시글 id로 studygroup에서 조회
+
+    // 인기 스터디 게시글 조회
     @Query("SELECT sg FROM StudyGroup sg WHERE sg.id IN (SELECT s.studyGroup.id FROM StudyMembership s WHERE s.user.id = (SELECT u.id FROM User u WHERE u.kakaoUserNumber = :kakaoNum))")
     Page<StudyGroup> findByKakaonum(@Param("kakaoNum") String kakaoNum, Pageable pageable);
+
+    @Query("SELECT s FROM StudyGroup s " +
+            "LEFT JOIN StudyGroupMember m ON s.id = m.studyGroup.id " +
+            "WHERE s.status = :status " +
+            "GROUP BY s " +
+            "ORDER BY COUNT(m) DESC, s.createdAt DESC")
+    Page<StudyGroup> findPopularByApplicationCount(@Param("status") StatusEnum status, Pageable pageable);
 }
-
-
 
