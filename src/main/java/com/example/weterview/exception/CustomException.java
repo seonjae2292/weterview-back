3d7706267860de11db6d758cd7f47533815c46d6
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-04 21:49:00 +0900

    feat: 커스텀 예외 클래스 추가

---
+++ src/main/java/com/example/weterview/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.example.weterview.exception;
+
+import com.example.weterview.enums.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
