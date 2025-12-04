9a424ebbe6119f47c0c1035e47ba39f9c696d0fc
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-04 15:14:33 +0900

    feat(entity): 객체 생성시 Builder 패턴 사용하기 위한 annotation 추가

--- src/main/java/com/example/weterview/entity/User.java
+++ src/main/java/com/example/weterview/entity/User.java
@@ -1,8 +1,7 @@
 package com.example.weterview.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,8 +11,11 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 @Table(name = "users")
-@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
