25d4cdc6adc51de74fbca43f205a9b4a57fdf8e8
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-04 22:00:12 +0900

    feat: 커스터 코드, forbidden 추가

--- src/main/java/com/example/weterview/enums/ErrorCode.java
+++ src/main/java/com/example/weterview/enums/ErrorCode.java
@@ -1,4 +1,4 @@
-package com.example.weterview.enums;
+package com.example.weterview.dto.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,25 +7,25 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    // 400 BAD_REQUEST
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
-    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "파라미터가 유효하지 않습니다."),
-    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다."),
 
-    // 401 UNAUTHORIZED
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    // Global
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GLOBAL_500", "서버 내부 오류가 발생했습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "GLOBAL_400", "입력값이 올바르지 않습니다."),
 
-    // 403 FORBIDDEN
-    FORBIDDEN(HttpStatus.FORBIDDEN, "해당 리소스에 접근할 권한이 없습니다"),
+    // FORBIDDEN
+    FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH_403", "해당 리소스에 접근 권한이 없습니다"),
 
-    // 404 NOT_FOUND
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
-    STUDY_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 스터디 그룹입니다."),
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시글입니다."),
+    // Auth
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_401", "인증되지 않은 사용자입니다."),
 
-    // 500 INTERNAL_SERVER_ERROR
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "존재하지 않는 사용자입니다."),
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "USER_400", "이미 존재하는 닉네임입니다."),
+
+    // StudyGroup
+    STUDY_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY_404", "존재하지 않는 스터디 그룹입니다.");
 
     private final HttpStatus httpStatus;
+    private final String code;
     private final String message;
 }
