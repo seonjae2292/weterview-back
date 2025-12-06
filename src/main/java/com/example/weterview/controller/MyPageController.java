d7686c2403432c65a4d489fd1d95bb4d564717e9
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-06 16:07:22 +0900

    refac(controller): Http 상태 코드, 결과 값 반환만 책임지도록

--- src/main/java/com/example/weterview/controller/MyPageController.java
+++ src/main/java/com/example/weterview/controller/MyPageController.java
@@ -3,9 +3,8 @@ package com.example.weterview.controller;
 import com.example.weterview.config.CustomUserDetails;
 import com.example.weterview.dto.common.ApiResponse;
 import com.example.weterview.dto.myPage.request.UpdateNicknameReq;
-import com.example.weterview.dto.myPage.response.GetHostedStudyGroupRes;
-import com.example.weterview.dto.myPage.response.GetJoinedStudyGroupRes;
-import com.example.weterview.dto.myPage.response.MyPageRes;
+import com.example.weterview.dto.myPage.response.*;
+import com.example.weterview.dto.studyGroup.response.GetStudyGroupApplyMemberRes;
 import com.example.weterview.service.MyPageService;
 import com.example.weterview.service.StudyGroupService;
 import jakarta.validation.Valid;
@@ -75,48 +74,53 @@ public class MyPageController {
     // 스터디 그룹 신청 수락
     @PostMapping("/accept/{userId}/{studyGroupId}")
     public ApiResponse<?> acceptJoinStudyGroup(
-            @PathVariable Long userId, @PathVariable String studyGroupId) {
-        myPageService.acceptJoinStudyGroup(userId, studyGroupId);
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @PathVariable Long userId, @PathVariable long studyGroupId) {
+        myPageService.acceptJoinStudyGroup(
+                customUserDetails.getUser(), userId, studyGroupId);
         return ApiResponse.success();
     }
-
-    // 스터디 그룹 상세 정보 조회
-    @GetMapping("/detail/{studyGroupId}")
-    public ApiResponse<?> getStudyGroupDetail(@PathVariable String studyGroupId) {
-        return studyGroupService.getStudyGroupDetail(studyGroupId);
-    }
-
-    // 스터디 그룹 신청자 조회
-
-    /// 스터디 그룹 상세 정보에서 자신이 개설한 정보 확인
-    @GetMapping("/applied/list/{studyGroupId}")
-    public ApiResponse<?> getAppliedStudyGroup(@PathVariable String studyGroupId) {
-        return studyGroupService.getAppliedStudyGroup(studyGroupId);
-    }
-
 
     // 스터디 그룹 신청 거절
     @PostMapping("/reject/{userId}/{studyGroupId}")
     public ApiResponse<?> rejectJoinStudyGroup(
-            @PathVariable Long userId, @PathVariable String studyGroupId) {
-        return myPageService.rejectJoinStudyGroup(userId, studyGroupId);
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @PathVariable Long userId, @PathVariable Long studyGroupId) {
+        myPageService.refuseJoinStudyGroup(
+                customUserDetails.getUser(), userId, studyGroupId);
+        return ApiResponse.success();
     }
 
-    // 좋아요한 게시글 조회
+    // 내가 좋아요한 게시글 조회
     @GetMapping("/likes/posts")
-    public ApiResponse<?> getLikePost(
-            @RequestHeader("Authorization") String jwt,
-            @RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber,
+    public ApiResponse<Page<GetLikedPostRes>> getLikePost(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestParam(value = "pageNumber", defaultValue = "1") int pageNumber,
             @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
-        return myPageService.getLikePost(jwt, pageNumber - 1, pageSize);
+        Page<GetLikedPostRes> response =
+                myPageService.getLikedPosts(customUserDetails.getUser(), pageNumber - 1, pageSize);
+        return ApiResponse.success(response);
     }
 
     // 댓글단 게시글 조회
     @GetMapping("/commented/posts")
-    public ApiResponse<?> getCommentedPost(
-            @RequestHeader("Authorization") String jwt,
-            @RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber,
+    public ApiResponse<Page<GetCommentedStudyGroupRes>> getCommentedPost(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestParam(value = "pageNumber", defaultValue = "1") int pageNumber,
             @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
-        return myPageService.getCommentedPost(jwt, pageNumber - 1, pageSize);
+        Page<GetCommentedStudyGroupRes> response =
+                myPageService.getCommentedPosts(customUserDetails.getUser(), pageNumber, pageSize);
+        return ApiResponse.success(response);
+    }
+
+    // 스터디 그룹에 신청한 신청자 목록 조회
+    @GetMapping("/applied/list/{studyGroupId}")
+    public ApiResponse<Page<GetStudyGroupApplyMemberRes>> getAppliedStudyGroup(
+            @PathVariable Long studyGroupId,
+            @RequestParam(value = "pageNumber", defaultValue = "1") int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        Page<GetStudyGroupApplyMemberRes> response =
+                myPageService.getAppliedStudyGroup(studyGroupId, pageNumber - 1, pageSize);
+        return ApiResponse.success(response);
     }
 }
