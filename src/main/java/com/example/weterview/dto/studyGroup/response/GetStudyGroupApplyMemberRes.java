70bbce7d39a65e4cb20505734b0321689bb6243e
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-06 16:07:55 +0900

    refac(dto): builder pattern으로 응답 객체 생성 로직 추가

--- src/main/java/com/example/weterview/dto/studyGroup/response/GetStudyGroupApplyMemberRes.java
+++ src/main/java/com/example/weterview/dto/studyGroup/response/GetStudyGroupApplyMemberRes.java
@@ -1,17 +1,33 @@
 package com.example.weterview.dto.studyGroup.response;
 
+import com.example.weterview.dto.myPage.response.GetJoinedStudyGroupRes;
+import com.example.weterview.entity.StudyGroupMember;
 import com.example.weterview.enums.studyMembership.JoinEnum;
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetStudyGroupApplyMemberRes {
     private Long userId;
     private String kakaoUserNumber;
     private String nickname;
     private String kakaoEmail;
     private String gender;
-    private LocalDateTime createdAt;
     private String status;
+
+    public static GetStudyGroupApplyMemberRes from(StudyGroupMember studyGroupMember) {
+        return GetStudyGroupApplyMemberRes.builder()
+                .userId(studyGroupMember.getUser().getId())
+                .kakaoUserNumber(studyGroupMember.getUser().getKakaoUserNumber())
+                .nickname(studyGroupMember.getUser().getNickname())
+                .kakaoEmail(studyGroupMember.getUser().getKakaoEmail())
+                .gender(studyGroupMember.getUser().getGender())
+                .status(String.valueOf(studyGroupMember.getJoin()))
+                .build();
+    }
+
 }
