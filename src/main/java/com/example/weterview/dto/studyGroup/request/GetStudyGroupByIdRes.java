0b4eeda3cf034783b49532a1504f3bc43749d39d
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-09 16:29:46 +0900

    feat(dto): entity -> dto

--- src/main/java/com/example/weterview/dto/studyGroup/request/GetStudyGroupByIdRes.java
+++ src/main/java/com/example/weterview/dto/studyGroup/request/GetStudyGroupByIdRes.java
@@ -1,20 +1,20 @@
 package com.example.weterview.dto.studyGroup.request;
 
+import com.example.weterview.entity.StudyGroup;
 import com.example.weterview.enums.studyGroup.FieldEnum;
 import com.example.weterview.enums.studyGroup.LocationEnum;
 import com.example.weterview.enums.studyGroup.StatusEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
+@Getter
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetStudyGroupByIdRes {
-    private String id;
+    private Long id;
     private FieldEnum field;
     private StatusEnum status;
     private String title;
@@ -31,5 +31,27 @@ public class GetStudyGroupByIdRes {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     @JsonProperty("isLiked")
-    private boolean isLiked;
+    private boolean isLiked = false;
+
+    public static GetStudyGroupByIdRes from(StudyGroup studyGroup, boolean isLiked) {
+        return GetStudyGroupByIdRes.builder()
+                .id(studyGroup.getId())
+                .field(studyGroup.getField())
+                .status(studyGroup.getStatus())
+                .title(studyGroup.getTitle())
+                .subTitle(studyGroup.getSubTitle())
+                .recruitingNumber(studyGroup.getRecruitingNumber())
+                .totalNumber(studyGroup.getTotalNumber())
+                .startDate(studyGroup.getStartDate())
+                .endDate(studyGroup.getEndDate())
+                .location(studyGroup.getLocation())
+                .description(studyGroup.getDescription())
+                .schedule(studyGroup.getSchedule())
+                .joinCondition(studyGroup.getJoinCondition())
+                .contact(studyGroup.getContact())
+                .createdAt(studyGroup.getCreatedAt())
+                .updatedAt(studyGroup.getUpdatedAt())
+                .isLiked(isLiked)
+                .build();
+    }
 }
