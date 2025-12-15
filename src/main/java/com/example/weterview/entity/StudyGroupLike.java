03c0b77fb601b754b62b328860c207f93c3f62dc
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-15 12:22:45 +0900

    refac(service&entity): builder 패턴과 정적 팩토리 메서드를 사용

--- src/main/java/com/example/weterview/entity/StudyGroupLike.java
+++ src/main/java/com/example/weterview/entity/StudyGroupLike.java
@@ -1,8 +1,9 @@
 package com.example.weterview.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -13,14 +14,15 @@ import java.time.LocalDateTime;
         @Index(name = "idx_user_liked_created", columnList = "user_id, is_liked, created_at")
 })
 @EntityListeners(AuditingEntityListener.class)
-@Data
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudyGroupLike {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "study_group_like_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
@@ -36,5 +38,25 @@ public class StudyGroupLike {
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
+    @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @Builder
+    private StudyGroupLike(User user, StudyGroup studyGroup, boolean isLiked) {
+        this.user = user;
+        this.studyGroup = studyGroup;
+        this.isLiked = isLiked;
+    }
+
+    public static StudyGroupLike create(User user, StudyGroup studyGroup) {
+        return StudyGroupLike.builder()
+                .user(user)
+                .studyGroup(studyGroup)
+                .isLiked(true)
+                .build();
+    }
+
+    public void toggle(){
+        this.isLiked = !this.isLiked;
+    }
 }
--- src/main/java/com/example/weterview/service/StudyGroupLikeService.java
+++ src/main/java/com/example/weterview/service/StudyGroupLikeService.java
@@ -4,6 +4,11 @@ import com.example.weterview.dto.common.ApiResponse;
 import com.example.weterview.entity.StudyGroup;
 import com.example.weterview.entity.StudyGroupLike;
 import com.example.weterview.entity.User;
+import com.example.weterview.enums.ErrorCode;
+import com.example.weterview.exception.CustomException;
+import com.example.weterview.repository.StudyGroupLikeRepository;
+import com.example.weterview.repository.StudyGroupRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,50 +17,24 @@ import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
+@AllArgsConstructor
 public class StudyGroupLikeService {
+    private final StudyGroupRepository studyGroupRepository;
+    private final StudyGroupLikeRepository studyGroupLikeRepository;
 
     // 게시글 좋아요
-    public ApiResponse<?> likeStudyGroup(String studyGroupId, String jwt) {
-        String kakaoUserNumber = jwtUtil.getKakaoUserNumFromToken(jwt);
-
-        User user = userRepository.findByKakaoUserNumber(kakaoUserNumber)
-                .orElseThrow(() -> new IllegalArgumentException("없는 회원정보 입니다."));
+    @Transactional
+    public void likeStudyGroup(User principalUser, String studyGroupId) {
         StudyGroup studyGroup = studyGroupRepository.findById(Long.parseLong(studyGroupId))
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글 입니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.STUDY_GROUP_NOT_FOUND));
 
-        Optional<StudyGroupLike> studyGroupLike = studyGroupLikeRepository.findByStudyGroupAndUser(studyGroup, user);
+        Optional<StudyGroupLike> studyGroupLike =
+                studyGroupLikeRepository.findByStudyGroupAndUser(studyGroup, principalUser);
 
         if (studyGroupLike.isPresent()) {
-            studyGroupLike.get().setLiked(true);
-            studyGroupLikeRepository.save(studyGroupLike.get());
+            studyGroupLike.get().toggle();
         } else {
-            StudyGroupLike like = new StudyGroupLike();
-            like.setUser(user);
-            like.setStudyGroup(studyGroup);
-            like.setLiked(true);
-            studyGroupLikeRepository.save(like);
+            studyGroupLikeRepository.save(StudyGroupLike.create(principalUser, studyGroup));
         }
-
-        return ApiResponse.ok(null, "좋아요 성공");
     }
-
-    // 게시글 좋아요 취소
-    public ApiResponse<?> unlikeStudyGroup(String studyGroupId, String jwt) {
-        String kakaoUserNumber = jwtUtil.getKakaoUserNumFromToken(jwt);
-
-        User user = userRepository.findByKakaoUserNumber(kakaoUserNumber)
-                .orElseThrow(() -> new IllegalArgumentException("없는 회원정보 입니다."));
-        StudyGroup studyGroup = studyGroupRepository.findById(Long.parseLong(studyGroupId))
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글 입니다."));
-
-        StudyGroupLike studyGroupLike = studyGroupLikeRepository.findByStudyGroupAndUser(studyGroup, user)
-                .orElseThrow(() -> new IllegalArgumentException("없는 게시글입니다."));
-
-        studyGroupLike.setUpdatedAt(LocalDateTime.now());
-        studyGroupLike.setLiked(false);
-        studyGroupLikeRepository.save(studyGroupLike);
-
-        return ApiResponse.ok(null, "좋이요 취소 성공");
-    }
-
 }
