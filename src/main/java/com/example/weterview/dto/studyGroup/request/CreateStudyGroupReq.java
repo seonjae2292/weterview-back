2d1c7f3b6ea4bccd0b97b14394c100228e65baf5
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-19 21:36:30 +0900

    refac(dto): 스터디 그룹 게시글 생성 시에 vo를 조립하여 StudyGroup 객체를 만드는 책임을 request dto에서

--- src/main/java/com/example/weterview/dto/studyGroup/request/CreateStudyGroupReq.java
+++ src/main/java/com/example/weterview/dto/studyGroup/request/CreateStudyGroupReq.java
@@ -1,21 +1,51 @@
 package com.example.weterview.dto.studyGroup.request;
 
+import com.example.weterview.entity.User;
+import com.example.weterview.entity.studyGroup.StudyGroup;
+import com.example.weterview.entity.studyGroup.vo.StudyCapacity;
+import com.example.weterview.entity.studyGroup.vo.StudyContent;
+import com.example.weterview.entity.studyGroup.vo.StudyPeriod;
 import com.example.weterview.enums.studyGroup.FieldEnum;
 import com.example.weterview.enums.studyGroup.LocationEnum;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CreateStudyGroupReq {
-    private FieldEnum field;
     private String title;
     private String subTitle;
+    private FieldEnum field;
+    private String joinCondition;
+    private String description;
+    private String contact;
+    private String schedule;
+
     private Integer currentMemberCount;
     private Integer maxMemberCount;
+
     private String startDate;
     private String endDate;
+
     private LocationEnum location;
-    private String description;
-    private String schedule;
-    private String joinCondition;
-    private String contact;
+
+    public StudyGroup toEntity(User user) {
+        StudyContent content = StudyContent.builder()
+                .title(this.title)
+                .subTitle(this.subTitle)
+                .field(this.field)
+                .joinCondition(this.joinCondition)
+                .description(this.description)
+                .contact(this.contact)
+                .schedule(this.schedule)
+                .build();
+
+        StudyPeriod period = StudyPeriod.of(
+                LocalDateTime.parse(this.startDate),
+                LocalDateTime.parse(this.endDate));
+
+        StudyCapacity capacity = StudyCapacity.from(this.maxMemberCount);
+
+        return StudyGroup.create(user, content, period, capacity, this.location);
+    }
 }
