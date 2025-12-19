eb3b0d46692d2d4389a00f4b130ff62a76fa9999
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-19 21:55:46 +0900

    feat(vo): 유효성 검증 로직 추가, schedule 추가

--- src/main/java/com/example/weterview/entity/studyGroup/vo/StudyContent.java
+++ src/main/java/com/example/weterview/entity/studyGroup/vo/StudyContent.java
@@ -1,11 +1,14 @@
 package com.example.weterview.entity.studyGroup.vo;
 
+import com.example.weterview.enums.ResultCode;
 import com.example.weterview.enums.studyGroup.FieldEnum;
 import com.example.weterview.enums.studyGroup.StatusEnum;
+import com.example.weterview.exception.CustomException;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
@@ -20,9 +23,6 @@ public class StudyContent {
 
     private String description;
 
-    @Comment(value = "스터디 상세 일정 ex) 매주 토요일 오후 2시 ~ 5시")
-    private String schedule;
-
     @Comment(value = "스터디에 참가할 수 있는 조건 ex) java에 대한 기본기가 있으면 좋겠습니다.")
     private String joinCondition;
 
@@ -32,14 +32,25 @@ public class StudyContent {
     @Enumerated(EnumType.STRING)
     private FieldEnum field;
 
+    @Comment(value = "스터디 그룹 스케줄 상세")
+    private String schedule;
+
+    @Builder
     public StudyContent(String title, String subTitle, String description,
-                        String schedule, String joinCondition, String contact, FieldEnum field) {
+                        String joinCondition, String contact, String schedule, FieldEnum field) {
+        if (title == null || title.isBlank()) {
+            throw new CustomException(ResultCode.INVALID_INPUT_VALUE, "제목은 필수입니다.");
+        }
+        if (description == null || description.isBlank()) {
+            throw new CustomException(ResultCode.INVALID_INPUT_VALUE, "내용은 필수입니다.");
+        }
+
         this.title = title;
         this.subTitle = subTitle;
         this.description = description;
-        this.schedule = schedule;
         this.joinCondition = joinCondition;
         this.contact = contact;
+        this.schedule = schedule;
         this.field = field;
     }
 }
