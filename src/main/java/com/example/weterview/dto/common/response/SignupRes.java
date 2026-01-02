25b2d246ffa0721b07091242943920f1a3ff1562
seonjae2292@gmail.com <seonjae2292@gmail.com>
2026-01-02 18:20:49 +0900

    remove(dto): 삭제

--- src/main/java/com/example/weterview/dto/common/response/SignupRes.java
+++
@@ -1,9 +0,0 @@
-package com.example.weterview.dto.common.response;
-
-import lombok.*;
-
-@Data
-@EqualsAndHashCode(callSuper = false)
-public class SignupRes extends OurMemberDto {
-    private String accessToken;
-}
